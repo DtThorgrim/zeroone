e32599da4c8bcb6990534c55e0106cef400c53c8
Thorgrim <paul.garnier4@gmail.com>
2016-12-16 00:22:55 +0100

    Rename and filtre auto generated code

--- nfcmanager/src/main/java/com/dtthorgrim/trousseaumanager/MainActivity.java
+++ nfcmanager/src/main/java/com/dtthorgrim/trousseaumanager/MainActivity.java
@@ -3,7 +3,7 @@ package com.dtthorgrim.trousseaumanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DebugActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
